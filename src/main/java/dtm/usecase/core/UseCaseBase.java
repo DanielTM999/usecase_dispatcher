87f62da18817272a8633effd04d33889ed15f42a
= <=>
2024-08-14 22:50:22 -0300

    Changes to be committed: 	modified:   src/main/java/dtm/usecase/core/UseCaseBase.java

--- src/main/java/dtm/usecase/core/UseCaseBase.java
+++ src/main/java/dtm/usecase/core/UseCaseBase.java
@@ -2,7 +2,20 @@ package dtm.usecase.core;
 
 public abstract class UseCaseBase {
     public UseCaseBase(){}
+
     protected void throwsMethod() throws RuntimeException{
         throw new RuntimeException();
+    }
+
+    protected void throwsMethod(String msg) throws RuntimeException{
+        throw new RuntimeException(msg);
+    }
+
+    protected void throwsMethod(Throwable th) throws RuntimeException{
+        throw new RuntimeException(th);
+    }
+
+    protected void throwsMethod(String msg, Throwable th) throws RuntimeException{
+        throw new RuntimeException(msg, th);
     }
 }
